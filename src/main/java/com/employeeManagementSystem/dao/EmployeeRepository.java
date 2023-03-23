c4978d39c9abe7062e51d69de354cf0b0e733f61
parince <konrad.tupta@gmail.com>
2023-03-23 11:15:27 +0100

    Implemented auto sorting

--- src/main/java/com/employeeManagementSystem/dao/EmployeeRepository.java
+++ src/main/java/com/employeeManagementSystem/dao/EmployeeRepository.java
@@ -3,5 +3,10 @@ package com.employeeManagementSystem.dao;
 import com.employeeManagementSystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
