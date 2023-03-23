8f771605bb722a8e8bb73bd2c3476d7c9113f012
parince <konrad.tupta@gmail.com>
2023-03-23 12:10:18 +0100

    Implemented update function

--- src/main/java/com/employeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/com/employeeManagementSystem/controller/EmployeeController.java
@@ -5,12 +5,8 @@ import com.employeeManagementSystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -44,6 +40,20 @@ public class EmployeeController {
 
 		theModel.addAttribute("employee", theEmployee);
 
+		return "employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId,
+									Model theModel) {
+
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
 		return "employee-form";
 	}
 
